0828f439aaf7be9f25b22619b51edfae58e77ba2
ROHIIL ZHALKE <37410753+rohilzalke1995@users.noreply.github.com>
2021-04-11 14:34:27 +0530

    Add files via upload

---
+++ merge sort.py
@@ -0,0 +1,41 @@
+#O(NlogN)
+def merge_sort(arr):
+    
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+        
+        merge_sort(left)
+        merge_sort(right)
+        
+        i=0
+        j=0
+        k=0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i = i+1
+            else:
+                arr[k] = right[j]
+                j = j+1
+            k = k+1
+        
+        
+        #for the remaining elements
+        while i < len(left):
+            arr[k] = left[i]
+            i = i+1
+            k = k+1
+        while j < len(right):
+            arr[k] = right[j]
+            j = j+1
+            k = k+1
+    print(arr)
+    
+A = [54,26,93,17,77,31,44,55,20]
+merge_sort(A)
+
+        
+        
